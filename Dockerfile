a111212eb85f53c75dbf7c044ac109e6393928a4
Alexey Vetrov <a.a.vetrov@edu.mtuci.ru>
2026-05-03 18:11:25 +0300

    Add tests and workflows

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.11
 
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-
 WORKDIR /app
+
+RUN pip install --no-cache-dir fastapi uvicorn pydantic-settings pydantic httpx pytest
+
 COPY src ./src
 
 ENTRYPOINT [ "python", "-m", "src.main" ]
--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,22 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nobody@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={"name": "New Guy", "email": "newguy@mail.com"})
+    assert response.status_code == 201
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={"name": "Ivan Clone", "email": users[0]['email']})
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': users[1]['email']})
+    assert response.status_code == 204
